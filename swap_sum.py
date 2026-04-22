97b1dddd9e8e2f6c11caca21edd7bf7c6e24a266
Rishika Sahu <sahurishika7@gmail.com>
2026-04-22 13:44:49 -0700

    Programming Problem 2

--- swap_sum.py
+++ swap_sum.py
@@ -1,4 +1,24 @@
 # Programming Problem 2
 
 def swap_sum(A, B):
-    return
+    """
+    >>> A = [1, 6, 50]
+    >>> B = [4, 24, 35]
+    >>> swap_sum(A, B)
+    (1, 0)
+    """
+    check = sum(B) - sum(A) - 10
+    if check % 2 != 0:
+        return None
+    target = check // 2
+    a_i = 0
+    b_j = 0
+    while a_i < len(A) and b_j < len(B):
+        current = B[b_j] - A[a_i]
+        if current == target:
+            return (a_i, b_j)
+        elif current < target:
+            b_j += 1
+        else:
+            a_i += 1
+    return None
