ae7fa1ec3ad381d8fe48d30c33353b1160797dd0
Rishika Sahu <sahurishika7@gmail.com>
2026-05-20 23:17:29 -0700

    pp2 hw7

--- cluster.py
+++ cluster.py
@@ -1,8 +1,46 @@
+import dsc40graph
+
+def cluster_help(u, graph, status, non_edges):
+    nodes = []
+    status[u] = "pending"
+
+    for v in graph.neighbors(u):
+        if (status[v] == "undiscovered" and (u, v) not in non_edges and (v, u) not in non_edges):
+            nodes += cluster_help(v, graph, status, non_edges)
+
+    status[u] = "visited"
+    nodes.append(u)
+    return nodes
+
+
 def cluster(graph, weights, level):
     """
+    >>> edges = [("a", "b"), ("b", "c"), ("c", "d"), ("a", "d")]
+    >>> graph = dsc40graph.UndirectedGraph()
+    >>> for edge in edges:
+    ...     graph.add_edge(*edge)
     >>> def weights(x, y):
-    ... x, y = (x, y) if x < y else (y, x)
-    ... return {("a", "b"): 1, ("b", "c"): .3, ("c", "d"): .9, ("a", "d"): .2}[(x, y)]
-    >>> cluster.cluster(graph, weights, 0.4)
-    frozenset([frozenset(['a', 'b']), frozenset(['c', 'd'])])
+    ...     x, y = (x, y) if x < y else (y, x)
+    ...     return {
+    ...         ("a", "b"): 1,
+    ...         ("b", "c"): .3,
+    ...         ("c", "d"): .9,
+    ...         ("a", "d"): .2
+    ...     }[(x, y)]
+
+    >>> cluster(graph, weights, 0.4)
+    frozenset({frozenset({'a', 'b'}), frozenset({'c', 'd'})})
     """
+    status = {node: "undiscovered" for node in graph.nodes}
+    clusters = []
+    non_edges = []
+
+    for edge in graph.edges:
+        if weights(edge[0], edge[1]) < level:
+            non_edges.append(edge)
+
+    for node in graph.nodes:
+        if status[node] == "undiscovered":
+            clusters.append(frozenset(cluster_help(node, graph, status, non_edges)))
+
+    return frozenset(clusters)
